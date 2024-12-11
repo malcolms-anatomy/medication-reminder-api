739db5898299fae79bf9a5f51d5ca3312f522a84
malcolms-anatomy <malcolmihere@outlook.com>
2024-12-11 02:35:36 +0100

    Updated the models.py file

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class MedicationReminder(models.Model):
+    user_email = models.EmailField()
+    medication_name = models.CharField(max_length=255)
+    dose = models.CharField(max_length=50)
+    time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.medication_name} for {self.user_email}"
